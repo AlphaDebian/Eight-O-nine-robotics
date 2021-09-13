80ddeab5ee801baa165902568e1546b0d785ea4b
Mayank Hirpara <37448949+AlphaDebian@users.noreply.github.com>
2021-09-13 23:49:01 +0530

    Add files via upload

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/user/container_ws/src
+# Build directory: /home/user/container_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("artpark")
+subdirs("eightonine_description")
